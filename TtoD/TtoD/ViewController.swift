1ea46a411d88af6236ae4d77be8ee463b3574bc6
Slimikus <slimikus@gmail.com>
2016-04-11 09:52:36 +0300

    убрана ландшавтная ориентация определены элементы на экране

--- TtoD/TtoD/ViewController.swift
+++ TtoD/TtoD/ViewController.swift
@@ -9,19 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        
-        
-        
-        
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var Objem: UITextField!
+    @IBOutlet weak var Skoroct: UITextField!
+    @IBOutlet weak var Result: UILabel!
+    @IBAction func Raschet(sender: UIButton) {
     }
 
 
